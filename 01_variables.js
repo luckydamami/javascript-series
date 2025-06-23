f898dcd479c19c4dbf2d77fb3e9535d1066af235
luckydamami <luckydamamii2410@gmail.com>
2025-06-23 15:02:20 +0530

    update js file

--- 01_variables.js
+++ 01_variables.js
@@ -1,4 +1,6 @@
-let accountId = 12746;
+//------------------- Variables ------------------
+
+/*let accountId = 12746;
 const accountEmail = "user123@gmail.com";
 var accountName = "Nikhil"; // var keyword not using because of functional and block scope problem
 
@@ -8,4 +10,25 @@ accountId = 65348;
 //accountEmail = "name@gmail.com";
 accountCity = "jodhpur";
 
-console.table([accountId, accountEmail, accountName, accountCity]);
+console.table([accountId, accountEmail, accountName, accountCity]);*/
+
+//------------------- Datatypes ------------------
+
+"use strict"; //treate all js code as newer version
+
+//alert(3+3)   //we are using nodejs, not browser
+
+/*console.log(3 + 3); //code Readablity must high
+console.log("Javascript!");
+
+/*---------- Premitive
+Number - any Number 
+BigInt - above the 3 digit numbers
+String - set of characters
+Boolean - true/false
+Symbol - unique value 
+undefined - jiski koi value defined na ho 
+null - standalone/empty value */
+
+/*Non-premitive
+Object*/
